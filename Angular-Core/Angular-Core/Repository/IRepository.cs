7cfefbacbab6f9314d717307ecb37b3060c16558
HelenHertsij <helen.hertsij@yandex.ua>
2018-10-24 14:32:20 +0300

    add Repository

---
+++ Angular-Core/Angular-Core/Repository/IRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AngularCore.Repository
+{
+    public interface IRepository<T> : IDisposable
+        where T : class
+    {
+        /// <summary>
+        /// Get all records from a table
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> GetAll();
+
+        /// <summary>
+        /// Get the record from a table by id
+        /// </summary>
+        /// <returns></returns>
+        T GetById(int id);
+
+        /// <summary>
+        /// Add a new record to a table
+        /// </summary>
+        /// <param name="item">the record wich need to added</param>
+        /// <returns></returns>
+        T Create(T item);
+
+        /// <summary>
+        /// Update a recod Employees table by id
+        /// </summary>
+        /// <param name="id">id of record wich need to update</param>
+        /// <param name="item">a record wich update</param>
+        /// <returns></returns>
+        bool Update(int id, T item);
+
+        /// <summary>
+        /// Remove a record from a table by id
+        /// </summary>
+        /// <param name="id">id of record wich delete</param>
+        /// <returns></returns>
+        bool Delete(int id);
+
+    }
+}
---
+++ Angular-Core/Angular-Core/Repository/ProductRepository.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using AngularCore.Models;
+using System.Threading.Tasks;
+
+namespace AngularCore.Repository
+{
+    public class ProductRepository : IRepository<Product>
+    {
+        ApplicationDbContext db;
+
+        /// <summary>
+        /// The property of disposed DataContext
+        /// </summary>
+        private bool disposed = false;
+
+        /// <summary>
+        /// Get all records from a table
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Product> GetAll()
+        {
+            return this.db.Products;
+
+        }
+        /// <summary>
+        /// Get the record from a table by id
+        /// </summary>
+        /// <returns></returns>
+        public Product GetById(int id)
+        {
+            Product product= db.Products.Find(id);
+            return product;
+        }
+
+        /// <summary>
+        /// Add a new record to a table
+        /// </summary>
+        /// <param name="item">the record wich need to added</param>
+        /// <returns></returns>
+        public Product Create(Product item)
+        {
+            try
+            {
+                this.db.Products.Add(item);
+                this.db.SaveChanges();
+                return item;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// Update a recod Employees table by id
+        /// </summary>
+        /// <param name="id">id of record wich need to update</param>
+        /// <param name="item">a record wich update</param>
+        /// <returns></returns>
+        public bool Update(int id, Product item)
+        {
+            Product p = db.Products.Find(id);
+            try
+            {
+                p.Name = item.Name;
+                p.Company = item.Company;
+                this.db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Remove a record from Employees table by id
+        /// </summary>
+        /// <param name="id">id of record wich delete</param>
+        /// <returns></returns>
+        public bool Delete(int id)
+        {
+            Product pr = this.db.Products.Find(id);
+            try
+            {
+                this.db.Remove(pr);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+
+        }
+
+        /// <summary>
+        /// The Dispose method
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    this.db.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        /// <summary>
+        /// The Dispose(true) method 
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
